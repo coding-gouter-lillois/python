75354ff9214f5714c3e30212c727e4586c226a7d
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2018-09-07 20:13:34 -0400

    Update guessTheNumber.py

--- Vianney/guessTheNumber.py
+++ Vianney/guessTheNumber.py
@@ -1,9 +1,6 @@
 #Importation du fichier qui permet d'avoir des nombres aléatoirs
 import random
 
-#La variable 'number' obtient une valeur comprise entre 1 & 10
-number = random.randint(1, 10)
-
 #Variable du nombre d'essais
 tries = 1
 
@@ -16,19 +13,24 @@ print("Hello", username + ".")
 #Demande au joueur si il veut jouer (J'espère que oui, c'est le but du programme ^^")
 question = input("Do you want to play a game? [Y/N]")
 if question == "n":
-	print("Oh... Ok ;(")
-else:
-	while 1:
-		print("I'm thinking of a number between 1 and 10")
-		guess = int(input("Have a guess :"))
+    print("Oh... Ok ;(")
 
-#Boucle infinie
-	while guess != number:
-		tries = tries + 1
-		if guess < number:
-			guess = int(input("Guess highter :"))
-		if guess > number:
-			guess = int(input("Guess lower :"))
-	else:
-		print("Well done")
+#Si la réponse est 'y' (Yes)
+if question == "y":
+    while 1:
+        #La variable 'number' obtient une valeur comprise entre 1 & 10
+        number = random.randint(1, 10)
+        #Annonce du début du jeu
+        print("I'm thinking of a number between 1 and 10")
+        guess = int(input("Have a guess :"))
+
+        #Boucle infinie
+        while guess != number:
+            tries = tries + 1
+            if guess < number:
+                guess = int(input("Guess highter :"))
+            if guess > number:
+                guess = int(input("Guess lower :"))
+        else:
+            print("Well done!")
 
